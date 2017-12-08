fda330f3866cfdcfd610b10551249edd1a382d2d
maurolepore <maurolepore@gmail.com>
2017-12-08 11:03:32 -0500

    Remove dependency on dplyr, magrittr and tibble.

--- R/conflicts.R
+++ R/conflicts.R
@@ -29,14 +29,14 @@ fgeo_conflict_message <- function(x) {
     right = "fgeo_conflicts()"
   )
 
-  pkgs <- x %>% purrr::map(~ gsub("^package:", "", .))
-  others <- pkgs %>% purrr::map(`[`, -1)
+  pkgs <- purrr::map(x, ~ gsub("^package:", "", .))
+  others <- purrr::map(pkgs, `[`, -1)
   other_calls <- purrr::map2_chr(
     others, names(others),
     ~ paste0(crayon::blue(.x), "::", .y, "()", collapse = ", ")
   )
 
-  winner <- pkgs %>% purrr::map_chr(1)
+  winner <- purrr::map_chr(pkgs, 1)
   funs <- format(paste0(crayon::blue(winner), "::", crayon::green(paste0(names(x), "()"))))
   bullets <- paste0(
     crayon::red(cli::symbol$cross), " ", funs,
@@ -52,12 +52,12 @@ print.fgeo_conflicts <- function(x, ..., startup = FALSE) {
   cli::cat_line(fgeo_conflict_message(x))
 }
 
-#' @importFrom magrittr %>%
 confirm_conflict <- function(packages, name) {
   # Only look at functions
-  objs <- packages %>%
-    purrr::map(~ get(name, pos = .)) %>%
-    purrr::keep(is.function)
+  objs <- purrr::keep(
+    purrr::map(packages, ~ get(name, pos = .)),
+    is.function
+  )
 
   if (length(objs) <= 1)
     return()
