1e59f2b76262c0f1befd96baf836136b3e0af029
maurolepore <maurolepore@gmail.com>
2018-11-19 14:06:38 -0500

    Test fgeo_dependencies().

--- tests/testthat/test-fgeo_dependencies.R
+++ tests/testthat/test-fgeo_dependencies.R
@@ -1,3 +1,23 @@
+context("fgeo_dependencies")
+
+test_that("is sensitive to `matches`", {
+  expect_true(any(grepl("^cli$", fgeo_dependencies())))
+  expect_false(any(grepl("^cli$", fgeo_dependencies(matches = "fgeo"))))
+  expect_true(any(grepl("^fgeo$", fgeo_dependencies(matches = "fgeo"))))
+  expect_true(any(grepl("^fgeo.x$", fgeo_dependencies(matches = "fgeo"))))
+})
+
+test_that("is sensitive to `include_fgeo`", {
+  expect_false(
+    any(grepl("^fgeo$", fgeo_dependencies("fgeo", include_self = FALSE)))
+  )
+  expect_true(
+    any(grepl("^fgeo.x$", fgeo_dependencies("fgeo", include_self = FALSE)))
+  )
+})
+
+
+
 context("find_deps_locally")
 
 root_from_testthat <- "../../../"
