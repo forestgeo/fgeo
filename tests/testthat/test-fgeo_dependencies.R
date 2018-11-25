7716eb6553f07836d84db2ea2d2c2774dec62f05
maurolepore <maurolepore@gmail.com>
2018-11-24 19:10:27 -0500

    Clean: Remove function for local use that has now a better, general alternative in fgeo.install.

--- tests/testthat/test-fgeo_dependencies.R
+++ tests/testthat/test-fgeo_dependencies.R
@@ -16,34 +16,3 @@ test_that("is sensitive to `include_fgeo`", {
   )
 })
 
-
-
-context("find_deps_locally")
-
-root_from_testthat <- "../../../"
-# root_from_testthat <- "../"  # run this line if running manually.
-
-test_that("returns the expected string", {
-  skip_on_travis()
-
-  dependencies_found <- fgeo:::find_deps_locally(
-    "fgeo.tool",
-    root = root_from_testthat,
-    fgeo_pkgs = fgeo_core()
-  )
-  matching <- grepl("fgeo.map", dependencies_found)
-
-  expect_true(any(matching))
-})
-
-test_that("errs with wrong input", {
-  skip_on_travis()
-
-  expect_error(
-    fgeo:::find_deps_locally(
-      "fgeo.tool",
-      root = root_from_testthat,
-      fgeo_pkgs = "missing_pkg"
-    )
-  )
-})
