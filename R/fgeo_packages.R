401573f1da9d18dba3837feddca4345a28ad6085
Mauro Lepore <maurolepore@gmail.com>
2018-12-26 16:22:41 -0500

    Extract code to build reference into pkgdoc (#111)
    * Replace fgeo.map with fgeo.plot.
    * Extract functions to build reference into pkgdoc.

--- R/fgeo_packages.R
+++ R/fgeo_packages.R
@@ -16,10 +16,45 @@ fgeo_packages <- function(include_self = TRUE) {
 }
 
 fgeo_to_attach <- function() {
-  c(
-    "fgeo.x",
-    "fgeo.map",
-    "fgeo.analyze",
-    "fgeo.tool"
-  )
+ fgeo_packages(include_self = FALSE)
+}
+
+
+
+#' List package dependencies (imports and suggests) of __fgeo__.
+#'
+#' @param matches String to `grep()` the output.
+#' @param include_self Include fgeo in the list?
+#' @param section Either "Imports" or "Suggests".
+#' @return A string.
+#'
+#' @family functions for developers
+#' @noRd
+fgeo_dependencies <- function(matches = NULL,
+  include_self = TRUE,
+  section = c("Imports", "Suggests")) {
+  section <- section[[1]]
+  raw <- utils::packageDescription("fgeo")[[section]]
+  pulled <- strsplit(raw, ",")[[1]]
+  parsed <- gsub("^\\s+|\\s+$", "", pulled)
+  names <- vapply(strsplit(parsed, " +"), "[[", 1, FUN.VALUE = character(1))
+
+  if (section == "Suggests") {
+    if (include_self) {
+      message(
+        "Ignoring argument `ignore_self` (it makes no sense for 'Suggests')."
+      )
+    }
+    return(names)
+  }
+
+  if (include_self) {
+    names <- c(names, "fgeo")
+  }
+
+  if (!is.null(matches)) {
+    names <- grep(matches, names, value = TRUE)
+  }
+
+  names
 }
--- tests/testthat/test-fgeo_packages.R
+++ tests/testthat/test-fgeo_packages.R
@@ -10,7 +10,7 @@ test_that("is sensitive to `include_self`", {
       "fgeo.x",
       "fgeo.tool",
       "fgeo.analyze",
-      "fgeo.map",
+      "fgeo.plot",
       "fgeo"
   )
   expect_sorted_equal(fgeo_packages(), install_these)
@@ -20,10 +20,37 @@ test_that("is sensitive to `include_self`", {
 
   attach_these <- c(
     "fgeo.x",
-    "fgeo.map",
+    "fgeo.plot",
     "fgeo.analyze",
     "fgeo.tool"
   )
   expect_sorted_equal(fgeo_to_attach(), attach_these)
 })
 
+
+
+context("fgeo_dependencies")
+
+test_that("is sensitive to `matches`", {
+  expect_true(any(grepl("^cli$", fgeo_dependencies())))
+  expect_false(any(grepl("^cli$", fgeo_dependencies(matches = "fgeo"))))
+  expect_true(any(grepl("^fgeo$", fgeo_dependencies(matches = "fgeo"))))
+  expect_true(any(grepl("^fgeo.x$", fgeo_dependencies(matches = "fgeo"))))
+})
+
+test_that("is sensitive to `include_fgeo`", {
+  expect_false(
+    any(grepl("^fgeo$", fgeo_dependencies("fgeo", include_self = FALSE)))
+  )
+  expect_true(
+    any(grepl("^fgeo.x$", fgeo_dependencies("fgeo", include_self = FALSE)))
+  )
+})
+
+test_that("warns nonsenseical argument combination", {
+  expect_message(
+    fgeo_dependencies("fgeo", include_self = TRUE, section = "Suggests"),
+    "Ignoring"
+  )
+
+})
