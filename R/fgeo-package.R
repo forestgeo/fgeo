30b185da7524175c11a40e088767a760b4fe3c88
Mauro Lepore <maurolepore@gmail.com>
2019-02-12 10:13:40 -0500

    Suppress note. Closes #197.

--- R/fgeo-package.R
+++ R/fgeo-package.R
@@ -1,2 +1,10 @@
 #' @keywords internal
 "_PACKAGE"
+
+# Suppress R CMD check note
+#' @importFrom fgeo.x example_path
+#' @importFrom fgeo.tool pick_dbh_min
+#' @importFrom fgeo.analyze abundance
+#' @importFrom fgeo.plot sp
+NULL
+
